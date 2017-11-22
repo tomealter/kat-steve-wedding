3bcee02f70b582d7962763f7a2594848b6a73a37
Tommy Alter <tomealter@gmail.com>
2017-11-22 01:00:29 -0500

    completed animation and responsiveness for travel section

--- wp-content/themes/peacebone/js/scripts.js
+++ wp-content/themes/peacebone/js/scripts.js
@@ -27,17 +27,29 @@
   $('.image-grid__item').each( function() {
     var currentPhoto = this;
     
-    var tween = TweenMax.from(currentPhoto, .3, {
+    var tween_story = TweenMax.from(currentPhoto, .3, {
       x: '-=100', 
       'opacity': '0',
       ease:Power1.easeIn
     });
     
-    var scene = new ScrollMagic.Scene({triggerElement: currentPhoto, triggerHook: 0.9})
-      .setTween(tween)
+    var scene_story = new ScrollMagic.Scene({triggerElement: currentPhoto, triggerHook: 0.8})
+      .setTween(tween_story)
       .addTo(controller); 
   });
 
+
+  //Animation for travel seciton
+  var map = $('.section__map');
+  var mapMarker = $('.section__map-marker');
+
+  var tween_travel = new TimelineMax();
+  tween_travel.from(map, 0.5, {'opacity':0, y: -50})
+  .from(mapMarker, 0.5, {'opacity':0,  y: -200, ease:Back.easeOut}, '1');
+
+  var scene_travel = new ScrollMagic.Scene({triggerElement: '.section__map', triggerHook: 0.6})
+  .setTween(tween_travel)
+  .addTo(controller); 
 
 
 
