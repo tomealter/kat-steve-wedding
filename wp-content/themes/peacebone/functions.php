86519d2f97ef279c9011c05d9fb32e1070d61ce9
Tommy Alter <tomealter@gmail.com>
2017-12-18 23:34:26 -0500

    added things to go content type // added ttd section // refactored the travel section to include new content

--- wp-content/themes/peacebone/functions.php
+++ wp-content/themes/peacebone/functions.php
@@ -42,7 +42,7 @@ class StarterSite extends TimberSite {
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu();
 		$context['site'] = $this;
-		$context['events'] = Timber::get_posts('post_type=events');
+		$context['things_to_do'] = Timber::get_posts('post_type=things_to_do');
 		return $context;
 	}
 
